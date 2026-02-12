e3326b0a58e800dac1d89efb576f98b608ab3c24
ttsukuyomi3auka <kostiakarpenok@gmail.com>
2026-02-12 17:11:31 +0500

    music

---
+++ Assets/MusicManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public static MusicManager Instance;
+    
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
